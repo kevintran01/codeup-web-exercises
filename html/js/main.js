6afc2507efedd12be3090db7d5c6caae68db783f
kevintran01 <Kevintran04024@gmail.com>
2018-08-27 16:28:22 -0500

    "new updating file"

--- html/js/main.js
+++
@@ -1,9 +0,0 @@
-var myAge = prompt("How old are you?");
-function birthday() {
-    var message = "Happy Birthday! You are ";
-    message += ++myAge;
-    message += " years old.";
-    console.log(message);
-}
-
-birthday();
