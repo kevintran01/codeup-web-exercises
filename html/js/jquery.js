6df52397c5415b6333722097c4d86571682d3021
kevintran01 <Kevintran04024@gmail.com>
2018-08-27 15:48:10 -0500

    exercise jQuery

---
+++ html/js/jquery.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("li").hover(function () {
+        $("li").css("background-color", "pink");
+    });
+    $("h1").click(function () {
+        $("h1").css("background-color", "green");
+    });
+});
