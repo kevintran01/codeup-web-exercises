78bc29dd65fbb358f23f9b641ed111014f87cae4
kevintran01 <Kevintran04024@gmail.com>
2018-09-07 14:15:09 -0500

    update

--- js/es6.js
+++ js/es6.js
@@ -2,31 +2,31 @@
  * Complete the TODO items below
  */
 const users = [
-  {
-    name: 'Kevin',
-    email: 'KevinTran04024@Google.com',
-    languages: ['HTML', 'CSS', 'JavaScript']
-  },
-  {
-    name: 'Kevin',
-    email: 'KevinTran04024@Google.com',
-    languages: ['HTML', 'CSS', 'Javascript']
-  },
-  {
-    name: 'Kevin',
-    email: 'KevinTran04024@Google.com',
-    languages: ['HTML', 'CSS', 'Javascript']
-  },
-  {
-    name: 'Kevin',
-    email: 'KevinTran04024@Google.com',
-    languages: ['HTML', 'CSS', 'Javascript']
-  },
-  {
-    name: 'Kevin',
-    email: 'KevinTran04024@Google.com',
-    languages: ['HTML', 'CSS', 'javascript']
-  }
+    {
+        name: 'Kevin',
+        email: 'KevinTran04024@Google.com',
+        languages: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        name: 'Kevin',
+        email: 'KevinTran04024@Google.com',
+        languages: ['HTML', 'CSS', 'Javascript']
+    },
+    {
+        name: 'Kevin',
+        email: 'KevinTran04024@Google.com',
+        languages: ['HTML', 'CSS', 'Javascript']
+    },
+    {
+        name: 'Kevin',
+        email: 'KevinTran04024@Google.com',
+        languages: ['HTML', 'CSS', 'Javascript']
+    },
+    {
+        name: 'Kevin',
+        email: 'KevinTran04024@Google.com',
+        languages: ['HTML', 'CSS', 'javascript']
+    }
 ];
 
 // TODO: fill in your name and email and add some programming languages you know
@@ -36,12 +36,14 @@ const users = [
 const name = 'Kevin';
 const email = 'KevinTran04024@Gmail.com';
 const languages = ['HTML', 'CSS', 'Javascript'];
+//Reassigning the value of name //(name = 'Tran';)
+// Returns error because const cannot be change
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+    name: name,
+    email: email,
+    languages: languages
 });
 
 // TODO: replace `var` with `let` in the following variable declarations
@@ -49,30 +51,35 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach = (user) => emails.push(user.email);
-users.forEach = (user) => names.push(user.name);
+users.forEach = (user => emails.push(user.email));
+users.forEach = (user => names.push(user.name));
+//another way
+users.forEach(user => {
+    emails.push(user.email);
+    names.push(user.name);
+});
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
-  // TODO: rewrite the code below to use object destructuring assignment
-  //       note that you can also use destructuring assignment in the function
-  //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+users.forEach(function (user) {
+    // TODO: rewrite the code below to use object destructuring assignment
+    //       note that you can also use destructuring assignment in the function
+    //       parameter definition
+    const {email, languages, name} = user;
 
-  // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+    // TODO: rewrite the assignment below to use template strings
+    developers.push(`${name}'s email is ${email} ${name} knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+for (const developer of developers) {
 
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
+    // TODO: rewrite the assignment below to use template strings
+    list += `<li>${developer}</li>`;
+}
 list += '</ul>';
+
+document.getElementById("list").innerHTML = list;
