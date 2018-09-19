0adce8e81c6468bda556264c28f7a4e848750674
kevintran01 <Kevintran04024@gmail.com>
2018-09-19 18:26:35 -0500

    practice

---
+++ learning/JavaScript/test1.js
@@ -0,0 +1,27 @@
+var myVarible;
+myVarible = 10;
+console.log(myVarible);
+
+var myVar2 = "Hello";
+console.log(myVar2);
+myVar2 = "Hello Again";
+console.log(myVar2);
+
+var myVar = 10;
+
+//myVar = myVar + 2
+myVar += 2;
+console.log(myVar);
+
+var youLikeMeat = true;
+var myNum = 5;
+
+//code block is the code you write between the curly brackets
+if (youLikeMeat) {
+    console.log("Here is the meat menu");
+}
+if (myNum > 6) {
+    console.log("myNum is greater than 6");
+} else {
+    console.log("myNum is not greater than 6");
+}
