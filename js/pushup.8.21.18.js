8ad1a1de712edd84e488390e800eec1d19848164
kevintran01 <Kevintran04024@gmail.com>
2018-10-03 12:14:17 -0500

    update

--- js/pushup.8.21.18.js
+++ js/pushup.8.21.18.js
@@ -1,29 +1,19 @@
-//factorial(5) ->
-// example: 5! = 5*4*3*2*1
-
-// var factorial = function (num) {
-//     var product = 1;
-//     for (var i = num; i > 1; i--) {
-//         num *= i;
-//     }
-//     return num;
-// }
-// console.log(factorial(1));
-// console.log(factorial(2));
-// console.log(factorial(3));
-// console.log(factorial(4));
-// console.log(factorial(5));
-// console.log(factorial(6));
-// console.log(factorial(7));
-// console.log(factorial(8));
-// console.log(factorial(9));
-// console.log(factorial(10));
-
-var summation = function (num) {
-    var sum = 0;
-    for (var i = num; i>0; i--) {
-        sum +=i;
+function fizzBuzz (val) {
+    for (var = i;
+    i < 101;
+    i++
+)
+    {
+        if (i % 15 == 0) console.log("FizzBuzz");
+        else if (i % 3 == 0) console.log("Fizz");
+        else if (i % 5 == 0) console.log("Buzz");
+        else console.log(i);
     }
-    return sum;
+
+    getFib(number) {
+        array = [1,1];
+        for (6 + i = 1, i < number, i++) {
+            array.push[number + (number -1 )];
+        } return array
+    }
 }
-console.log(summation(5));
--- learning/JavaScript/basicJavaScript.js
+++ learning/JavaScript/basicJavaScript.js
@@ -1,27 +1,196 @@
-var myVarible;
-myVarible = 10;
-console.log(myVarible);
+/** TODO Basic JavaScript: Comment Your JavaScript Code
 
-var myVar2 = "Hello";
-console.log(myVar2);
-myVar2 = "Hello Again";
-console.log(myVar2);
+/** here are two ways to write comments in JavaScript:
 
-var myVar = 10;
+Using // will tell JavaScript to ignore the remainder of the text on the current line:
 
-//myVar = myVar + 2
-myVar += 2;
-console.log(myVar);
+        // This is an in-line comment.
 
-var youLikeMeat = false;
-var myNum = 10;
+You can make a multi-line comment beginning with /* and ending with
 
-//code block is the code you write between the curly brackets
-if (youLikeMeat) {
-    console.log("Here is the meat menu");
-}
-else if (myNum > 6) {
-    console.log("myNum is greater than 6");
-} else {
-    console.log("myNum is not greater than 6");
-}
+/* This is a
+multi-line comment */
+
+/*TODO Create a // style comment that contains at least five letters.
+
+// This is a in-line comment in JavaScript
+
+TODO Create a multi-line style comment that contains at least five letters.
+
+/* This is a
+multi-line comment in JavaScript.  */
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
+/** TODO Basic JavaScript: Declare JavaScript Variables
+
+ In computer science, data is anything that is meaningful to the computer.
+ JavaScript provides seven different data types which are undefined, null, boolean, string, symbol, number, and object.
+
+ For example, computers distinguish between numbers, such as the number 12, and strings,
+ such as "12", "dog", or "123 cats", which are collections of characters.
+ Computers can perform mathematical operations on a number, but not on a string.
+
+ Variables allow computers to store and manipulate data in a dynamic fashion.
+ They do this by using a "label" to point to the data rather than using the data itself.
+ Any of the seven data types may be stored in a variable.
+
+ Variables are similar to the x and y variables you use in mathematics,
+ which means they're a simple name to represent the data we want to refer to.
+ Computer variables differ from mathematical variables in that they can store different values at different times.
+
+ We tell JavaScript to create or declare a variable by putting the keyword var in front of it, like so:
+
+ var ourName;
+ creates a variable called ourName. In JavaScript we end statements with semicolons.
+
+ Variable names can be made up of numbers, letters, and $ or _, but may not contain spaces or start with a number.
+
+TODO Use the var keyword to create a variable called myName */
+
+var myName;
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
+/** TODO Basic JavaScript: Storing Values with the Assignment Operator
+
+ In JavaScript, you can store a value in a variable with the assignment operator.
+
+ myVariable = 5;
+
+ This assigns the Number value 5 to myVariable.
+
+ Assignment always goes from right to left. Everything to the right of the = operator
+ is resolved before the value is assigned to the variable to the left of the operator.
+
+ myVar = 5;
+ myNum = myVar;
+
+ This assigns 5 to myVar and then resolves myVar to 5 again and assigns it to myNum.
+
+ TODO Assign the value 7 to variable a.
+
+ TODO Assign the contents of a to variable b. */
+
+var a = 7;
+
+console.log(b = a);
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
+/**TODO Basic JavaScript: Initializing Variables with the Assignment Operator
+
+ It is common to initialize a variable to an initial value in the same line as it is declared.
+
+ var myVar = 0;
+
+ Creates a new variable called myVar and assigns it an initial value of 0.
+
+ TODO Define a variable a with var and initialize it to a value of 9. */
+
+var a = 9;
+console.log(a);
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
+/** TODO Basic JavaScript: Understanding Uninitialized Variables
+
+ When JavaScript variables are declared, they have an initial value of undefined.
+ If you do a mathematical operation on an undefined variable your result will be NaN
+ which means "Not a Number". If you concatenate a string with an undefined variable,
+ you will get a literal string of "undefined".
+
+ TODO Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined. */
+
+// Initialize these three variables
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// undefined example
+var d;
+
+// Do not change code below this line
+
+console.log(a = a + 1);
+console.log(b = b + 5);
+console.log(c = c + " String!");
+console.log(d);
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
+/** TODO Basic JavaScript: Understanding Case Sensitivity in Variables
+ In JavaScript all variables and function names are case sensitive. This means that capitalization matters.
+
+ MYVAR is not the same as MyVar nor myvar. It is possible to have multiple distinct variables with the same
+ name but different casing. It is strongly recommended that for the sake of clarity, you do not use this language feature.
+
+ Best Practice
+ Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have
+ the first word in lowercase and the first letter of each subsequent word is capitalized.
+
+ Examples:
+
+ var someVariable;
+ var anotherVariableName;
+ var thisVariableNameIsSoLong;
+
+ TODO Modify the existing declarations and assignments so their names use camelCase. Do not create any new variables.
+
+ // Declarations
+ var StUdLyCapVaR;
+ var properCamelCase;
+ var TitleCaseOver;
+
+ // Assignments
+ STUDLYCAPVAR = 10;
+ PRoperCAmelCAse = "A String";
+ tITLEcASEoVER = 9000; */
+
+// Declarations
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+console.log(studlyCapVar);
+console.log(properCamelCase);
+console.log(titleCaseOver);
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
+/** TODO Basic JavaScript: Add Two Numbers with JavaScript
+
+ Number is a data type in JavaScript which represents numeric data.
+
+ Now let's try to add two numbers using JavaScript.
+
+ JavaScript uses the + symbol as addition operation when placed between two numbers.
+
+ Example
+
+ myVar = 5 + 10; // assigned 15
+
+ TODO Change the 0 so that sum will equal 20.
+
+ var sum = 10 + 0;                              */
+
+var sum = 10 + 10;
+
+console.log(sum);
+
+/* *******************************************************************************
+ ****************************************************************************** */
+
